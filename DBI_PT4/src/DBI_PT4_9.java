7162dfdb6c3fcfedff3788d2cc51c1fd0101fc0e
owntYA <fpotthoff@outlook.de>
2015-12-09 11:18:01 +0100

    7b
    Aufgabe b - check

--- DBI_PT4/src/DBI_PT4_9.java
+++ DBI_PT4/src/DBI_PT4_9.java
@@ -22,11 +22,13 @@ public class DBI_PT4_9 {
 		return result;
 	}
 	
-	public void payIn(Connection con, int accId, int tellerId, int branchId, int delta) throws SQLException
+	public void payIn(Connection con, int accId, int tellersId, int branchId, int delta) throws SQLException
 	{
 		Statement stm = con.createStatement();
 		ResultSet rs = null;
 		int result = 0;
+		
+		// UPDATE BALANCE@ BRANCHES
 		String txt = "select balance from branches where branchid = "+branchId;
 		stm.executeUpdate(txt);
 		rs = stm.getResultSet();
@@ -35,8 +37,33 @@ public class DBI_PT4_9 {
 	    		result = rs.getInt(1);
 	    		result += delta;
 	    }
-		txt = "update branches SET balance ="+result+" where accid"
+		txt = "update branches SET balance ="+result+" where branchid = "+branchId;
+		stm.executeUpdate(txt);
 		
+		// UPDATE BALANCE@ TELLERS
+		txt = "select balance from tellers where tellersid = "+tellersId;
+		stm.executeUpdate(txt);
+		rs = stm.getResultSet();
+		if( rs != null ) 
+	    {
+	    		result = rs.getInt(1);
+	    		result += delta;
+	    }
+		txt = "update tellers SET balance ="+result+" where tellersid = "+tellersId;
+		stm.executeUpdate(txt);
+		
+		// UPDATE BALANCE@ ACCOUNTS
+		txt = "select balance from accounts where accid = "+accId;
+		stm.executeUpdate(txt);
+		rs = stm.getResultSet();
+		if( rs != null ) 
+		{
+			result = rs.getInt(1);
+		  	result += delta;
+		}
+		txt = "update accounts SET balance ="+result+" where accid = "+accId;
+		stm.executeUpdate(txt);
+						
 	}
 	
 	public static void main(String[] args) throws SQLException {
