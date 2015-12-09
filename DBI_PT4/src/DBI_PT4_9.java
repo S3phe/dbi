43e903e5eff5a166530d6c629c1bb2359f84e56c
Sebastian H. <s@heun.de>
2015-12-09 10:03:07 +0100

    Erste Formulierung

--- DBI_PT4/src/DBI_PT4_9.java
+++ DBI_PT4/src/DBI_PT4_9.java
@@ -4,6 +4,15 @@ public class DBI_PT4_9 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Hier kommt Aufgabe9 rein
-	}
+		
+		// Kontostands-TX (ACCID eingeben und BALANCE zurückgeben
+		// Einzahlungs-TX (AACID,TELLERID,BRANCHID,DELTA eingeben. 
+		//	-	In "Branches" die Bilanzsumme Balance passend zur Branchid aktualisieren
+		//	-	In "Tellers" die Bilanzsumme Balance passend zur Tellerid aktualisieren
+		//	-	In "Accounts" den Kontostand BALANCE passend zur ACCID aktualisieren
+		//	-	
 
+		
+		
+	}
 }
