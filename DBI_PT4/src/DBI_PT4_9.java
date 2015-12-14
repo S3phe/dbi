15faef8afb8e56a381141081b6ac64ca7905816e
Sebastian H <s@heun.de>
2015-12-14 11:14:21 +0100

    Rückgabewerte, Eingaben etc
    Die SQL-Funktionen haben jetzt eine STRING-Rückgabe. Die Ausgabe in der
    Konsole wird nun von der Unterfunktion gebastelt. Das sorgt für einen
    klareren Programmaufbau.

--- DBI_PT4/src/DBI_PT4_9.java
+++ DBI_PT4/src/DBI_PT4_9.java
@@ -16,26 +16,27 @@ public class DBI_PT4_9 {
 		//Connection con = DriverManager.getConnection("jdbc:mariadb://localhost:3306/test","root", "dbi2015");
 		Connection con = DriverManager.getConnection("jdbc:mariadb://10.37.129.3:3306/benchmark","dbi", "dbi_pass");			
 		
+		System.out.println("Aufgabenstellung: Praktikumgsaufgabe 9\n");
 		menu(con);
 				
 	}
 	
-	public int getBalance(Connection con, int accId) throws SQLException
-	{
+	public static String kontostand(Connection con, Integer account) throws SQLException{
 		Statement stm = con.createStatement();
 		ResultSet rs = null;
 		int result = 0;
-		String txt = "SELECT balance FROM accounts WHERE accid ="+accId;
+		String txt = "SELECT balance FROM accounts WHERE accid ="+account;
 		stm.executeUpdate(txt);
 		rs = stm.getResultSet();
 	    while( rs != null ) 
 	    {
 	    		result = rs.getInt(1);
 	    }
-		return result;
+		return "Kontostand: "+result;
+		
 	}
 	
-	public static void payIn(Connection con, int accId, int tellersId, int branchId, int delta) throws SQLException
+	public static String einzahlung(Connection con, int accId, int tellersId, int branchId, int delta) throws SQLException
 	{
 		Statement stm = con.createStatement();
 		ResultSet rs = null;
@@ -77,55 +78,60 @@ public class DBI_PT4_9 {
 		}
 		txt = "UPDATE accounts SET balance ="+result+" WHERE accid = "+accId;
 		stm.executeUpdate(txt);
-		rs = null;	
+		rs = null;
+		return "Erfolgreich.";	
+	}
+	
+	public static String analyse(Connection con, Integer delta){
+		return null;
+		
+		
 	}
 	
 	public static void menu(Connection con) throws SQLException {
-		// Aufgabenstellung
-		System.out.println("Aufgabenstellung: Praktikumgsaufgabe 9\n");
-		// Auswahl
-		System.out.println();
-		System.out.println("1 Kontostands-TX");
-		System.out.println("2 Einzahlungs-TX");
-		System.out.println("3 Analyse-TX");
-		System.out.println("4 Beenden");
-		System.out.print("Eingabe: ");
-		int auswahl = Integer.parseInt(scanner.nextLine());
-		
-		switch(auswahl){
-			case 1: tx_kontostand(con); break;
-			case 2: tx_einzahlung(con); break;
-			case 3: tx_analyse(con); break;
-			case 4: System.exit(0); break;
-			default: menu(con); break;
+		while(true){
+			System.out.println();
+			System.out.println("1 Kontostands-TX");
+			System.out.println("2 Einzahlungs-TX");
+			System.out.println("3 Analyse-TX");
+			System.out.println("4 Beenden");
+			System.out.print("Eingabe: ");
+			int auswahl = Integer.parseInt(scanner.nextLine());
+			
+			switch(auswahl){
+				case 1: menu_kontostand(con); break;
+				case 2: menu_einzahlung(con); break;
+				case 3: menu_analyse(con); break;
+				case 4: System.exit(0); break;
+				default: menu(con); break;
+			}
 		}
 	}
 	
 	
-	public static void tx_kontostand(Connection con){
-		
+	public static void menu_kontostand(Connection con) throws SQLException{
+		System.out.print("Kontonummer (ACCID); ");
+		int accid = Integer.parseInt(scanner.nextLine());
+		System.out.print(kontostand(con,accid));
 	}
 	
-	public static void tx_einzahlung(Connection con) throws SQLException{
-		// n abfragen
+	public static void menu_einzahlung(Connection con) throws SQLException{
 		System.out.print("Geben Sie eine AccountID ein: ");
-		String acc = scanner.nextLine();
-		int accid = Integer.parseInt(acc);
+		int accid = Integer.parseInt(scanner.nextLine());
 		System.out.print("Geben Sie eine BranchID ein: ");
-		String branch = scanner.nextLine();
-		int branchid = Integer.parseInt(branch);
+		
+		int branchid = Integer.parseInt(scanner.nextLine());
 		System.out.print("Geben Sie eine TellersID ein: ");
-		String tellers = scanner.nextLine();
-		int tellersid = Integer.parseInt(tellers);
+		int tellersid = Integer.parseInt(scanner.nextLine());
 		System.out.print("Geben Sie ein Delta ein: ");
-		String value = scanner.nextLine();
-		int delta = Integer.parseInt(value);
-		scanner.close();			
-		payIn(con, accid, tellersid, branchid, delta);
+		int delta = Integer.parseInt(scanner.nextLine());	
+		System.out.println(einzahlung(con, accid, tellersid, branchid, delta));
 	}
 	
-	public static void tx_analyse(Connection con){
-		
+	public static void menu_analyse(Connection con){
+		System.out.print("Geben Sie den Differenzbetrag (Delta) ein: ");
+		int delta = Integer.parseInt(scanner.nextLine());
+		System.out.println(analyse(con,delta));
 	}
 	
 	
