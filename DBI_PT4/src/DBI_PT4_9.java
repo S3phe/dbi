c33470e2981f4e6979feb1ced04eb1071506ac4d
Sebastian Heun <s@heun.de>
2015-12-16 12:24:18 +0100

    Benchmark ergänzt, Menü umgebaut.

--- DBI_PT4/src/DBI_PT4_9.java
+++ DBI_PT4/src/DBI_PT4_9.java
@@ -3,17 +3,31 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Random;
 import java.util.Scanner;
 
 public class DBI_PT4_9 {
 	// Eingabe initialisieren
 	static Scanner scanner = new Scanner(System.in);
 	
+	
+	public static Random rand = new Random();
+	
+	public static String randomString (int length) {
+		
+		String chars="ABCDEFGH0123456789";
+		StringBuilder buf = new StringBuilder();
+		for (int i=0; i<length; i++) {
+			buf.append(chars.charAt(rand.nextInt(chars.length())));
+		}
+		return buf.toString();
+	}
+	
 	public static void main(String[] args) throws SQLException {
 		// TODO Auto-generated method stub
 	
-		Connection con = DriverManager.getConnection("jdbc:mariadb://localhost:3307/benchmark","root", "dbi2015");
-		//Connection con = DriverManager.getConnection("jdbc:mariadb://10.37.129.3:3306/benchmark","dbi", "dbi_pass");			
+		//Connection con = DriverManager.getConnection("jdbc:mariadb://localhost:3307/benchmark","root", "dbi2015");
+		Connection con = DriverManager.getConnection("jdbc:mariadb://10.37.129.3:3306/benchmark","dbi", "dbi_pass");			
 		menu(con);
 	}
 	
@@ -89,7 +103,7 @@ public class DBI_PT4_9 {
 		Statement stm = con.createStatement();
 		ResultSet rs = null;
 		
-		// ALLE TUPEL AUS HISTORY MIT DEM ÜBERGABEPARAM. DELTA
+		// ALLE TUPEL AUS HISTORY MIT DEM ï¿½BERGABEPARAM. DELTA
 		String txt = "SELECT * FROM history WHERE delta = "+delta;
 		stm.execute(txt);
 		rs = stm.getResultSet();
@@ -101,6 +115,127 @@ public class DBI_PT4_9 {
 		return counter;	
 	}
 	
+	protected static void db_optimize(Connection con) throws SQLException{
+		Statement sql = con.createStatement();
+		sql.execute("SET FOREIGN_KEY_CHECKS=0");
+		sql.execute("SET UNIQUE_CHECKS=0");
+		con.setAutoCommit(false);
+	}
+	
+	protected static void db_deoptimize(Connection con) throws SQLException{
+		Statement sql = con.createStatement();
+		sql.execute("SET FOREIGN_KEY_CHECKS=1");
+		sql.execute("SET UNIQUE_CHECKS=1");
+		con.setAutoCommit(true);
+	}
+	
+	public static void benchmark(Connection con) throws SQLException {
+		System.out.println("Statement erzeugen...");
+		
+		
+		// Komplette Benchmark Datenbank neu erstellen
+		Statement statement=con.createStatement();
+		
+		// Bestehende Datenbank lÃ¶schen, falls diese existiert und neu anlegen
+		System.out.println("Datenbank lÃ¶schen und neu anlegen...");
+		statement.execute("DROP DATABASE IF EXISTS benchmark");
+		statement.execute("CREATE DATABASE benchmark");
+		statement.execute("USE benchmark");
+		// Tabellen gemÃ¤ÃŸ Vorgabe anlegen
+		System.out.println("Tabellen erzeugen...");
+		statement.execute("CREATE TABLE branches (branchid int not null, branchname char(20) not null, balance int not null, address char(72) not null, primary key (branchid));");
+		statement.execute("CREATE TABLE accounts (accid int not null, name char(20) not null, balance int not null, branchid int not null, address char(68) not null, primary key (accid), foreign key (branchid) references branches (branchid));");
+		statement.execute("CREATE TABLE tellers (tellerid int not null, tellername char(20) not null, balance int not null, branchid int not null, address char(68) not null, primary key (tellerid), foreign key (branchid) references branches (branchid));");
+		statement.execute("CREATE TABLE history (accid int not null, tellerid int not null, delta int not null, branchid int not null, accbalance int not null, cmmnt char(30) not null, foreign key (accid) references accounts (accid), foreign key (tellerid) references tellers (tellerid), foreign key (branchid) references branches (branchid));"); 
+		
+		System.out.println("Datenbank optimieren");
+		db_optimize(con);
+		// 100 tps-Datenbank mit Inhalt fÃ¼llen
+		Integer n = 100;
+		System.out.println("Tabellen mit Inhalt fÃ¼llen... ("+n+"tps)");
+		
+		System.out.println("Fill Branches...");
+		// Fill Branches
+		StringBuilder build = new StringBuilder();
+		build.append("INSERT INTO branches (branchid, branchname, balance, address) VALUES ");
+		String branchname = randomString(20);
+		String address = randomString(72);
+
+		for (int i=1;i<=n;i++){
+			build.append("("+i+",'"+branchname+"',0,'"+address+"')");
+			if (i<n){
+				build.append(", ");
+				
+			}else{
+				build.append(";");
+			}
+			
+		}
+		statement.execute(build.toString());
+		build.delete(0,build.length());
+		System.out.println("Fill Accounts...");
+		
+		int 	branchid=0; 
+		String 	name = randomString(20);
+		address = randomString(68);
+		int accid=0;
+		
+		// Daten splitten, da RAM der VM (DBMS) nicht ausreicht
+		
+		// Angabe, wieviele Werte in einen String sollen:
+		int inserts_pro_durchgang = 1000000;
+		int insert_durchgaenge = (n*100000) / inserts_pro_durchgang;
+
+		for (int i=0;i<insert_durchgaenge;i++)
+		{	
+			build.append("INSERT INTO accounts (accid, name, balance, branchid, address) VALUES ");
+			for (int j=1;j<=inserts_pro_durchgang;j++)
+			{
+				branchid = rand.nextInt(n)+1;
+				accid=i*inserts_pro_durchgang + j;
+				build.append("("+accid+",'"+name+"',0,"+branchid+",'"+address+"')");
+				if (j<inserts_pro_durchgang){
+					build.append(", ");
+				}else{
+					build.append(";");
+				}
+			}
+			statement.execute(build.toString());
+			build.delete(0,build.length());
+			System.out.println("Fortschritt: "+i+"/"+insert_durchgaenge);
+		}
+		
+		build.delete(0,build.length());
+		System.out.println("Fill Tellers...");
+		
+		build.append("INSERT INTO tellers (tellerid, tellername, balance, branchid, address) VALUES ");
+		
+		branchid= 0; 
+		String 	tellername=randomString(20);
+		address = randomString(68);
+		
+		for (int i=1;i<=n*10;i++){
+			branchid= rand.nextInt(n)+1;
+			build.append("("+i+",'"+tellername+"',0,"+branchid+",'"+address+"')");	
+			if (i<n*10){
+				build.append(", ");
+				
+			}else{
+				build.append(";");
+			}
+		}
+		statement.execute(build.toString());
+		
+		con.commit();
+		System.out.println("Datenbank deoptimieren...");
+		db_deoptimize(con);
+		System.out.println("Datenbank ist vorbereitet. Bereit fÃ¼r Benchmark.");
+		
+		
+		
+		
+	}
+	
 	public static void menu(Connection con) throws SQLException {
 		// Aufgabenstellung
 		System.out.println("Aufgabenstellung: Praktikumgsaufgabe 9\n");
@@ -109,7 +244,8 @@ public class DBI_PT4_9 {
 		System.out.println("1 Kontostand-TX");
 		System.out.println("2 Einzahlung-TX");
 		System.out.println("3 Analyse-TX");
-		System.out.println("4 Beenden");
+		System.out.println("4 Benchmark");
+		System.out.println("5 Beenden");
 		System.out.print("Eingabe: ");
 		int auswahl = Integer.parseInt(scanner.nextLine());
 		
@@ -117,7 +253,8 @@ public class DBI_PT4_9 {
 			case 1: tx_kontostand(con); break;
 			case 2: tx_einzahlung(con); break;
 			case 3: tx_analyse(con); break;
-			case 4: System.exit(0); break;
+			case 4: benchmark(con); break;
+			case 5: System.exit(0); break;
 			default: menu(con); break;
 		}
 	}
@@ -129,7 +266,7 @@ public class DBI_PT4_9 {
 		int accid = Integer.parseInt(acc);
 		int output = getBalance(con, accid);
 		scanner.close();
-		System.out.println("Der momentane Kontostand des Konto's "+accid+" beträgt "+output+" Einheiten!");
+		System.out.println("Der momentane Kontostand des Konto's "+accid+" betraegt "+output+" Einheiten!");
 	}
 	
 	public static void tx_einzahlung(Connection con) throws SQLException{
@@ -148,7 +285,7 @@ public class DBI_PT4_9 {
 		int delta = Integer.parseInt(value);
 		scanner.close();			
 		int output = payIn(con, accid, tellersid, branchid, delta);
-		System.out.println("Der aktualisierte Kontostand des Konto's "+accid+" beträgt "+output+" Einheiten!");
+		System.out.println("Der aktualisierte Kontostand des Konto's "+accid+" betraegt "+output+" Einheiten!");
 	}
 	
 	public static void tx_analyse(Connection con) throws SQLException{
@@ -157,6 +294,6 @@ public class DBI_PT4_9 {
 		String s = scanner.nextLine();		
 		int delta = Integer.parseInt(s);
 		int amount = consistsDelta(con, delta);
-		System.out.println("Die Anzahl der Überweisungen mit dem Delta-Wert "+delta+" betragen: "+amount);
+		System.out.println("Die Anzahl der Ueberweisungen mit dem Delta-Wert "+delta+" betragen: "+amount);
 	}
 }
