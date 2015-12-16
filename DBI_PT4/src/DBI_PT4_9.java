42a1662cf0f1b77dcbda87f2ae34c78ce3c02dc6
owntYA <fpotthoff@outlook.de>
2015-12-16 13:36:59 +0100

    Aufräumaktion.

--- DBI_PT4/src/DBI_PT4_9.java
+++ DBI_PT4/src/DBI_PT4_9.java
@@ -10,12 +10,6 @@ import java.util.Scanner;
 public class DBI_PT4_9 {
 	// Eingabe initialisieren
 	static Scanner scanner = new Scanner(System.in);
-	
-	
-	// TEST VON SEBASTIAN
-	
-	
-	
 	public static Random rand = new Random();
 	
 	public static String randomString (int length) {
@@ -44,11 +38,8 @@ public class DBI_PT4_9 {
 	
 	public static void benchmark(Connection con) throws SQLException {
 		System.out.println("Statement erzeugen...");
-		
-		
 		// Komplette Benchmark Datenbank neu erstellen
 		Statement statement=con.createStatement();
-		
 		// Bestehende Datenbank löschen, falls diese existiert und neu anlegen
 		System.out.println("Datenbank löschen und neu anlegen...");
 		statement.execute("DROP DATABASE IF EXISTS benchmark");
@@ -142,13 +133,8 @@ public class DBI_PT4_9 {
 		con.commit();
 		System.out.println("Datenbank deoptimieren...");
 		db_deoptimize(con);
-		System.out.println("Datenbank ist vorbereitet. Bereit für Benchmark.");
-		
-		
-		//
-		
+		System.out.println("Datenbank ist vorbereitet. Bereit f�r Benchmark.");		
 	}
-	
 	
 	public static void txLoop()
 	{
@@ -195,27 +181,24 @@ public class DBI_PT4_9 {
 				tx_analyse(con);
 			}
 		}
-		
 		fasenTime = System.currentTimeMillis();
 		while(fasenTime < ausschwingfase)
 		{
 			while(runTime<35%)
 			{
-				txy
+				tx_kontostand(con);
 			}
 			
 			while(runtime<10%)
 			{
-				txy
+				tx_einzahlung(con);
 			}
 			
 			while(runTime<40%)
 			{
-				txyz
+				tx_analyse(con);
 			}
-		}
-		
-		
+		}	
 		runTime=System.currentTimeMillis()-startTime;
 		System.out.println("Fertig. (Laufzeit: "+runTime+" ms)");
 	}
@@ -317,79 +300,79 @@ public class DBI_PT4_9 {
 	}
 	
 	// Menuefuehrung
-		public static void menu(Connection con) throws SQLException, IOException {
-			while(true)
-			{
-				// Aufgabenstellung
-				System.out.println("Aufgabenstellung: Praktikumgsaufgabe 9\n");
-				// Auswahl
-				System.out.println("\n\n\n\n\n\n");
-				System.out.println("///////   MENU   ///////");
-				System.out.println("------------------------");
-				System.out.println("||||||||||||||||||||||||");
-				System.out.println("------------------------");
-				System.out.println("| - 1: KONTOSTAND-TX   |");
-				System.out.println("|----------------------|");
-				System.out.println("| - 2: EINZAHLUNG-TX   |");
-				System.out.println("|----------------------|");
-				System.out.println("| - 3: ANALYSE-TX      |");
-				System.out.println("|--------------------- |");
-				System.out.println("| - 4. BEENDEN         |");
-				System.out.println("|----------------------|");
-				System.out.print("Eingabe: ");
-				int auswahl = Integer.parseInt(scanner.nextLine());
-				
-				
-				switch(auswahl){
-					case 1: tx_kontostand(con); break;
-					case 2: tx_einzahlung(con); break;
-					case 3: tx_analyse(con); break;
-					case 4: System.exit(0); break;
-					default: menu(con); break;
-				}	
-			}
+	public static void menu(Connection con) throws SQLException, IOException {
+		while(true)
+		{
+			// Aufgabenstellung
+			System.out.println("Aufgabenstellung: Praktikumgsaufgabe 9\n");
+			// Auswahl
+			System.out.println("\n\n\n\n\n\n");
+			System.out.println("///////   MENU   ///////");
+			System.out.println("------------------------");
+			System.out.println("||||||||||||||||||||||||");
+			System.out.println("------------------------");
+			System.out.println("| - 1: KONTOSTAND-TX   |");
+			System.out.println("|----------------------|");
+			System.out.println("| - 2: EINZAHLUNG-TX   |");
+			System.out.println("|----------------------|");
+			System.out.println("| - 3: ANALYSE-TX      |");
+			System.out.println("|--------------------- |");
+			System.out.println("| - 4. BEENDEN         |");
+			System.out.println("|----------------------|");
+			System.out.print("Eingabe: ");
+			int auswahl = Integer.parseInt(scanner.nextLine());
+			
+			
+			switch(auswahl){
+				case 1: tx_kontostand(con); break;
+				case 2: tx_einzahlung(con); break;
+				case 3: tx_analyse(con); break;
+				case 4: System.exit(0); break;
+				default: menu(con); break;
+			}	
 		}
+	}
 		
-		public static void tx_kontostand(Connection con) throws SQLException, IOException{
-			System.out.println("-- Abfragen des Kontostands --\n\n");
-			System.out.print("Geben Sie eine AccountID ein: ");
-			String acc = scanner.nextLine();
-			int accid = Integer.parseInt(acc);
-			int output = getBalance(con, accid);
-			scanner.close();
-			System.out.println("Der momentane Kontostand des Konto's "+accid+" betraegt "+output+" Einheiten!");
-			resume();
-		}
+	public static void tx_kontostand(Connection con) throws SQLException, IOException{
+		System.out.println("-- Abfragen des Kontostands --\n\n");
+		System.out.print("Geben Sie eine AccountID ein: ");
+		String acc = scanner.nextLine();
+		int accid = Integer.parseInt(acc);
+		int output = getBalance(con, accid);
+		scanner.close();
+		System.out.println("Der momentane Kontostand des Konto's "+accid+" betraegt "+output+" Einheiten!");
+		resume();
+	}
 		
-		public static void tx_einzahlung(Connection con) throws SQLException, IOException{
-			System.out.println("-- Geld einzahlen --\n\n");
-			System.out.print("Geben Sie eine AccountID ein: ");
-			String acc = scanner.nextLine();
-			int accid = Integer.parseInt(acc);
-			System.out.print("Geben Sie eine BranchID ein: ");
-			String branch = scanner.nextLine();
-			int branchid = Integer.parseInt(branch);
-			System.out.print("Geben Sie eine TellersID ein: ");
-			String tellers = scanner.nextLine();
-			int tellersid = Integer.parseInt(tellers);
-			System.out.print("Geben Sie ein Delta ein: ");
-			String value = scanner.nextLine();
-			int delta = Integer.parseInt(value);
-			scanner.close();			
-			int output = payIn(con, accid, tellersid, branchid, delta);
-			System.out.println("Der aktualisierte Kontostand des Konto's "+accid+" betraegt "+output+" Einheiten!");
-			resume();
-		}
+	public static void tx_einzahlung(Connection con) throws SQLException, IOException{
+		System.out.println("-- Geld einzahlen --\n\n");
+		System.out.print("Geben Sie eine AccountID ein: ");
+		String acc = scanner.nextLine();
+		int accid = Integer.parseInt(acc);
+		System.out.print("Geben Sie eine BranchID ein: ");
+		String branch = scanner.nextLine();
+		int branchid = Integer.parseInt(branch);
+		System.out.print("Geben Sie eine TellersID ein: ");
+		String tellers = scanner.nextLine();
+		int tellersid = Integer.parseInt(tellers);
+		System.out.print("Geben Sie ein Delta ein: ");
+		String value = scanner.nextLine();
+		int delta = Integer.parseInt(value);
+		scanner.close();			
+		int output = payIn(con, accid, tellersid, branchid, delta);
+		System.out.println("Der aktualisierte Kontostand des Konto's "+accid+" betraegt "+output+" Einheiten!");
+		resume();
+	}
 		
-		public static void tx_analyse(Connection con) throws SQLException, IOException{
-			System.out.println("-- Geldeinzahlungen bereits vorhanden? --\n\n");
-			System.out.print("Geben Sie ein Delta ein: ");
-			String s = scanner.nextLine();		
-			int delta = Integer.parseInt(s);
-			int amount = consistsDelta(con, delta);
-			System.out.println("Die Anzahl der Ueberweisungen mit dem Delta-Wert "+delta+" betragen: "+amount);
-			resume();
-		}
+	public static void tx_analyse(Connection con) throws SQLException, IOException{
+		System.out.println("-- Geldeinzahlungen bereits vorhanden? --\n\n");
+		System.out.print("Geben Sie ein Delta ein: ");
+		String s = scanner.nextLine();		
+		int delta = Integer.parseInt(s);
+		int amount = consistsDelta(con, delta);
+		System.out.println("Die Anzahl der Ueberweisungen mit dem Delta-Wert "+delta+" betragen: "+amount);
+		resume();
+	}
 	
 	// Mainfunktion mit Aufruf des Menue's (enthaelt Steuerung des Programmfluss) //	
 	public static void main(String[] args) throws SQLException, IOException {
